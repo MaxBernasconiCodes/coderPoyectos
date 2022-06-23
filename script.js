be5641b611992a242583f1fe540e9fb69e89723e
“Max <max_bernasconi@hotmail.com>
2022-06-22 23:01:36 -0300

    some changes to layout

---
+++ script.js
@@ -0,0 +1,10 @@
+//check for loaded users on localhost
+//if not loaded, redirect to login
+//once with checekd user procede
+// fetch users properties
+// load landing with access to direferent pages of the site
+// The menu has options to add or search between 4 columns , Propiedades | Propietarios | Inquilinos | Contratos
+// each collumn has loaded their elements qith a button for more information
+// inside each item, the description of the item and the edit
+// a button for deletion || goes to modal for confirmation
+// a cancel button || goes for modal to confirm loosing changes
